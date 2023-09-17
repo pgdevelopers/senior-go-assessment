0aa188ad783a89e60eca49986be82de4c1b32be7
Josh Rose <joshrose@hey.com>
2023-09-17 08:40:12 -0400

    add area of improvement

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5"
@@ -12,16 +11,13 @@ import (
 func main() {
 	ctx := context.Background()
 	router := gin.New()
-	conn, err := pgx.Connect(ctx, "host=localhost user=postgres password=password database=postgres")
-	if err != nil {
-		log.Fatal(err)
-	}
+	conn, _ := pgx.Connect(ctx, "host=localhost user=postgres password=password database=postgres")
 
 	router.GET("/welcome", func(c *gin.Context) {
 		lastname := c.Query("lastname")
 		var name string
 		q := fmt.Sprintf("SELECT first_name FROM users WHERE last_name = '%s' LIMIT 1", lastname)
-		err = conn.QueryRow(ctx, q).Scan(&name)
+		err := conn.QueryRow(ctx, q).Scan(&name)
 		if err != nil {
 			c.String(404, "User not found")
 		}
